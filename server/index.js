839935ce512ae2f439f2ad5cbe735dbcffe6de1f
phamngocan2412 <an.207ct68609@gmail.com>
2023-07-20 00:17:57 +0700

    update file

--- server/index.js
+++ server/index.js
@@ -17,3 +17,15 @@ app.get('/hello', (req, res) => {
 app.use('/api/admin', require('./api/admin.js'));
 // apis
 app.use('/api/customer', require('./api/customer.js'));
+// deployment
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
